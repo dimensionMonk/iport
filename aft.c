41f913d55a3f6fcc0e1a64c5185871964f0acdfa
dimensionMonk <142955643+dimensionMonk@users.noreply.github.com>
2023-09-06 01:01:50 -0700

    Create aft.c
    somting? //about a relase // i do not sell code

---
+++ aft.c
@@ -0,0 +1,6 @@
+transitter:unkonwn :: clean~ -trsn : lsfb :[usb] 
+:so 'system + trans:mitter [usb] : lfsb 
+//comment : work"AFT
+\\COMMENT : CLEAN : MUTEX_MONEY_BOOT { return @ time
+int some : blance : figure : fix // handle :: MUTEX
+//money or morals @time -freeware
