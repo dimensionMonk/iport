1dda2ebe0f937679d1ee24fe5f27e91f97b7accd
dimensionMonk <142955643+dimensionMonk@users.noreply.github.com>
2023-09-06 00:28:18 -0700

    Create music.c
    new functionality

---
+++ music.c
@@ -0,0 +1,12 @@
+ai_control(laryBIRD) char LARY -binary :: //comment
+"dullity" = "relationships" = binary :: //comment\\
+"personilty" + "BIRD" CHARICTER :: "personality"
+"system_bird_sorit" char doc :: 'mind' + 'mission' = "BIRD'so
+make so :: doc 'doc' == "so" ' ++ function : mision ~
+system>>so* (445 : 545) ~ (632 : 422) == 'function' return[macro]
+system >> text >> source~ world::ai_control::"persona"
+radio :: transmitter :: class{#borad
+|| night :: theam //morals :: relsase~
+radio :: trasmmitter:: class{imusic : lary_bird "release :: chaticter
+unsinged :: trans :: mitter {cap} --pul;
+//release author: michael shannon b.na
